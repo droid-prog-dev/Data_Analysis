9d2fc9e2fae55a01e0a93ba29ac6d831142ddab9
droid-prog-dev <76625118+droid-prog-dev@users.noreply.github.com>
2021-06-29 18:17:16 -0500

    Add files via upload

--- data_analysis.py
+++ data_analysis.py
@@ -40,7 +40,7 @@ df_stock_sutures = stock_lenova.loc[stock_lenova['codigo'].str.startswith('SN')]
 
 df_stock_sutures.sort_values(by=['codigo'], ascending=True, inplace=True)
 
-st.dataframe(df_stock, width=1400, height=600)
+st.dataframe(df_stock, width=1800, height=800)
 st.write(f"Total stock value S/.{round(np.sum(df_stock['S/.']),2)}")
 
 st.subheader("Stock de suturas:")
@@ -64,7 +64,7 @@ vtasutures.drop(['year','month'], axis='columns', inplace=True)
 vtasutures.set_index('period', inplace=True)
 vtasutures.reset_index(inplace=True)
 
-st.dataframe(vtasutures.style.highlight_max(axis=0), width=1500, height=800)
+st.dataframe(vtasutures.style.highlight_max(axis=0), width=1800, height=800)
 fig0 = make_subplots(rows=2, cols=1)
 
 fig0.add_trace(go.Scatter(x=vtasutures.index, y=np.squeeze(vtasutures['total-sum'].values),
@@ -74,7 +74,7 @@ fig0.add_trace(go.Scatter(x=vtasutures.index, y=np.squeeze(vtasutures['priceun-m
 				mode='lines+markers',name='AVG - Price'), row=2, col=1)
 
 
-fig0.update_layout(height=800, width=1200, title_text=f"Total Sutures Monthly Sales in Soles")
+fig0.update_layout(height=800, width=1000, title_text=f"Total Sutures Monthly Sales in Soles")
 st.write(fig0)
 
 
